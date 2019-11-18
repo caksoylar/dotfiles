99ea776f35a4b46a6cd9e985f34235a95ce4313b
Cem Aksoylar <ceaksoyl@microsoft.com>
2019-11-17 21:37:05 -0800

    add missing kak install script

---
+++ kak/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ln -s "$(readlink -f $(dirname $0))" ~/.config/kak
+ln -s "$(readlink -f ~/.config/kak/kak-lsp)" ~/.config/kak-lsp
+
+mkdir -p ~/.config/kak/autoload/remote
+
+ln -s "$(type -p kak)"/../share/kak/autoload ~/.config/kak/autoload/system
+for url in \
+    "https://raw.githubusercontent.com/alexherbo2/auto-pairs.kak/master/rc/auto-pairs.kak" \
+    "https://raw.githubusercontent.com/Delapouite/kakoune-buffers/master/buffers.kak" \
+    "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
+    "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak"; do
+    wget "$url" -O ~/.config/kak/autoload/remote/$(basename "$url")
+done
+
+# TODO: add kak-lsp
