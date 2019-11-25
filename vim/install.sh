c437995e5675ad8dac14ebde3c30b51e40fdc2d6
Cem Aksoylar <cem@cem-desktop.localdomain>
2019-11-24 16:07:37 -0800

    vim: also install init.vim

--- vim/install.sh
+++ vim/install.sh
@@ -3,5 +3,9 @@
 ln -s "$(readlink -f $(dirname $0))" ~/.vim
 mkdir -p ~/.vim/{temp,undo}
 
+mkdir -p ~/.config/nvim
+ln -s ~/.vim/init.vim ~/.config/nvim/
+
 wget https://raw.githubusercontent.com/caksoylar/vim-jetpack/master/vjp -O ~/.vim/vjp
-bash ~/.vim/vjp ~/.vim/plugins.txt ~/.vim/pack/remote
+chmod u+x ~/.vim/vjp
+~/.vim/vjp ~/.vim/plugins.txt ~/.vim/pack/remote
