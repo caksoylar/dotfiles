59bcb77fb62b5c687f4e2d09cd08c5d8ca5d3c05
Cem Aksoylar <ceaksoyl@microsoft.com>
2019-11-27 11:51:43 -0800

    kak: add state-save

--- kak/install.sh
+++ kak/install.sh
@@ -10,6 +10,7 @@ for url in \
     "https://raw.githubusercontent.com/alexherbo2/auto-pairs.kak/master/rc/auto-pairs.kak" \
     "https://raw.githubusercontent.com/Delapouite/kakoune-buffers/master/buffers.kak" \
     "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
+    "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak" \
     "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak"; do
     wget "$url" -O ~/.config/kak/autoload/remote/$(basename "$url")
 done
