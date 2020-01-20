1ee2fbb854a4f1a9f3f6a667128a63a2e6288cda
Cem Aksoylar <cem@cem-desktop.localdomain>
2020-01-20 15:23:17 -0800

    kak: separate plugin script

--- kak/install.sh
+++ kak/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ln -s "$(readlink -f $(dirname $0))" ~/.config/kak
 ln -s "$(readlink -f ~/.config/kak/kak-lsp)" ~/.config/kak-lsp
@@ -6,13 +6,5 @@ ln -s "$(readlink -f ~/.config/kak/kak-lsp)" ~/.config/kak-lsp
 mkdir -p ~/.config/kak/autoload/remote
 
 ln -s $(readlink -f "$(dirname $(type -p kak))"/../share/kak/autoload) ~/.config/kak/autoload/system
-for url in \
-    "https://raw.githubusercontent.com/alexherbo2/auto-pairs.kak/master/rc/auto-pairs.kak" \
-    "https://raw.githubusercontent.com/Delapouite/kakoune-buffers/master/buffers.kak" \
-    "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
-    "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak" \
-    "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak"; do
-    wget "$url" -O ~/.config/kak/autoload/remote/$(basename "$url")
-done
 
-# TODO: add kak-lsp
+~/.config/kak/plugins.sh
---
+++ kak/plugins.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for url in \
+    "https://raw.githubusercontent.com/alexherbo2/auto-pairs.kak/master/rc/auto-pairs.kak" \
+    "https://raw.githubusercontent.com/Delapouite/kakoune-buffers/master/buffers.kak" \
+    "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
+    "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak" \
+    "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak"; do
+    wget --hsts-file= -nv "$url" -O ~/.config/kak/autoload/remote/$(basename "$url")
+done
