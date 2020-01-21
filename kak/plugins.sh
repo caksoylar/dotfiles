0f8b8cc8e2d65b7c016aa672ce4b7ff90de3392d
Cem Aksoylar <cem@cem-desktop.localdomain>
2020-01-20 17:08:22 -0800

    kak: update for surround

--- kak/plugins.sh
+++ kak/plugins.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for url in \
-    "https://raw.githubusercontent.com/alexherbo2/auto-pairs.kak/master/rc/auto-pairs.kak" \
+    "https://raw.githubusercontent.com/alexherbo2/surround.kak/master/rc/surround.kak" \
     "https://raw.githubusercontent.com/Delapouite/kakoune-buffers/master/buffers.kak" \
     "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
     "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak" \
