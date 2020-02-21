c26119428bf2c24c9f21256081db0ffc04d24e34
Cem Aksoylar <ceaksoyl@microsoft.com>
2020-02-21 11:46:02 -0800

    kak: add smarttab and shellcheck plugins

--- kak/plugins.sh
+++ kak/plugins.sh
@@ -4,7 +4,9 @@ for url in \
     "https://raw.githubusercontent.com/alexherbo2/surround.kak/master/rc/surround.kak" \
     "https://raw.githubusercontent.com/Delapouite/kakoune-buffers/master/buffers.kak" \
     "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
-    "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak" \
-    "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak"; do
+    "https://raw.githubusercontent.com/andreyorst/smarttab.kak/master/rc/smarttab.kak" \
+    "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak" \
+    "https://gitlab.com/Screwtapello/kakoune-shellcheck/-/raw/master/shellcheck.kak" \
+    "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak"; do
     wget --hsts-file= -nv "$url" -O ~/.config/kak/autoload/remote/$(basename "$url")
 done
