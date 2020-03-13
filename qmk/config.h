2b928bd64e0ac0262b14832570cebc7c5d1694bb
Cem Aksoylar <ceaksoyl@microsoft.com>
2020-03-12 17:33:36 -0700

    qmk: enable TAPPING_FORCE_HOLD

--- qmk/config.h
+++ qmk/config.h
@@ -27,5 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
+#define TAPPING_FORCE_HOLD
+
 // might need to flip encoder direction
 // #define ENCODER_DIRECTION_FLIP
