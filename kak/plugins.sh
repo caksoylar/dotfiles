5d35197507b79728114a9b9f9b77917d9942f9f6
Cem Aksoylar <ceaksoyl@microsoft.com>
2020-03-16 14:17:30 -0700

    kak: fix cursorline, add tool split

--- kak/plugins.sh
+++ kak/plugins.sh
@@ -6,6 +6,7 @@ for url in \
     "https://raw.githubusercontent.com/chambln/kakoune-readline/master/readline.kak" \
     "https://raw.githubusercontent.com/andreyorst/smarttab.kak/master/rc/smarttab.kak" \
     "https://raw.githubusercontent.com/occivink/kakoune-vertical-selection/master/vertical-selection.kak" \
+    "https://raw.githubusercontent.com/occivink/kakoune-find/master/find.kak" \
     "https://gitlab.com/Screwtapello/kakoune-shellcheck/-/raw/master/shellcheck.kak" \
     "https://gitlab.com/Screwtapello/kakoune-state-save/raw/master/state-save.kak"; do
     wget --hsts-file= -nv "$url" -O ~/.config/kak/autoload/remote/$(basename "$url")
