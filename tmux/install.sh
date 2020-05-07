1cc17371eb87907102fa875449b6b57b0e0a5c66
Cem Aksoylar <ceaksoyl@microsoft.com>
2020-05-07 15:40:40 -0700

    tmux: move config to .config/tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/.tmux
-ln -s "$(readlink -f $(dirname $0)/tmux.conf)" ~/.tmux/tmux.conf
-ln -s $(readlink ~/.tmux/tmux.conf) ~/.tmux.conf
+mkdir -p ~/.config/tmux/plugins
+ln -s "$(readlink -f $(dirname $0)/tmux.conf)" ~/.config/tmux/tmux.conf
 
-git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
-git clone --recursive https://github.com/Morantron/tmux-fingers ~/.tmux/plugins/tmux-fingers
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.config/tmux/plugins/tmux-resurrect
+git clone --recursive https://github.com/Morantron/tmux-fingers ~/.config/tmux/plugins/tmux-fingers
