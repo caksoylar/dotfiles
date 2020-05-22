09f360852ef94ad3cfc9c1f8b342e15fc5eb5845
Cem Aksoylar <ceaksoyl@microsoft.com>
2020-05-22 13:53:35 -0700

    kak: switch to plug.kak

--- kak/install.sh
+++ kak/install.sh
@@ -3,8 +3,8 @@
 ln -s "$(readlink -f $(dirname $0))" ~/.config/kak
 ln -s "$(readlink -f ~/.config/kak/kak-lsp)" ~/.config/kak-lsp
 
-mkdir -p ~/.config/kak/autoload/remote
+mkdir -p ~/.config/kak/plugins
+
+git clone https://github.com/andreyorst/plug.kak.git ~/.config/kak/plugins/plug.kak
 
 ln -s $(readlink -f "$(dirname $(type -p kak))"/../share/kak/autoload) ~/.config/kak/autoload/system
-
-~/.config/kak/kjp
