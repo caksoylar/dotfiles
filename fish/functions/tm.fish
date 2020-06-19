f63505275e6f87c386570c155469dda31cffe26d
Cem Aksoylar <cem@cem-desktop.localdomain>
2020-06-19 16:50:52 -0700

    extend tm and add kakoune-registers

--- fish/functions/tm.fish
+++ fish/functions/tm.fish
@@ -4,6 +4,6 @@ function tm
     else
         set session $argv[1]
     end
-    tmux new-session -A -s "$session"
+    tmux new-session -A -s "$session" $argv[2..-1]
 end
 complete -c tm -f -a "(tmux list-sessions -F '#{session_name}'\t'#{session_windows} windows (last attached #{t:session_last_attached})')"
