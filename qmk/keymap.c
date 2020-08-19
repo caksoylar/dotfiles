206b1f5aac62717439b66c6b935cf2149656fe46
Cem Aksoylar <caksoylar@gmail.com>
2020-08-18 22:09:32 -0700

    qmk: alt in game layer

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MPLY,          XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,
     //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                      KC_LCTL, KC_LSFT, KC_SPC,                    LT_SPC,  KC_ENT,  MO(_FUNC)
+                                      KC_LCTL, KC_LALT, KC_SPC,                    LT_SPC,  KC_ENT,  MO(_FUNC)
                                   // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
     ),
 
