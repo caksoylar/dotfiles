df8ddf711a205eecb7f2af1154f5a7646a62371b
Cem Aksoylar <caksoylar@gmail.com>
2021-01-26 13:58:41 -0800

    kak: fix plug repo

--- kak/install.sh
+++ kak/install.sh
@@ -5,6 +5,6 @@ ln -s "$(readlink -f ~/.config/kak/kak-lsp)" ~/.config/kak-lsp
 
 mkdir -p ~/.config/kak/plugins
 
-git clone https://github.com/andreyorst/plug.kak.git ~/.config/kak/plugins/plug.kak
+git clone https://github.com/robertmeta/plug.kak.git ~/.config/kak/plugins/plug.kak
 
 ln -s $(readlink -f "$(dirname $(type -p kak))"/../share/kak/autoload) ~/.config/kak/autoload/system
