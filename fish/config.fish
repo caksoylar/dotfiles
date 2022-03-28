7aea139ff7ca854992b1e272fa4632eabf24cca6
Cem Aksoylar <caksoylar@gmail.com>
2022-03-28 14:51:38 -0700

    fish: upgrade to 3.4.1

--- fish/config.fish
+++ fish/config.fish
@@ -65,18 +65,7 @@ alias ta="tmux attach-session"
 alias gst="vim '+G | only'"
 
 # old-style highlighting
-set fish_color_normal normal
-set fish_color_command --bold
-set fish_color_param cyan
-set fish_color_redirection brblue
-set fish_color_comment red
-set fish_color_error brred
-set fish_color_escape bryellow --bold
-set fish_color_operator bryellow
-set fish_color_end brmagenta
-set fish_color_quote yellow
-set fish_color_autosuggestion 555 brblack
-set fish_color_user brgreen
+fish_config theme choose termcolors
 
 # work stuff
 if test -f ~/.config/fish/work.fish
