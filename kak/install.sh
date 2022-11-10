b48096a592b0bf822d15d16b2509fd2bbb05f2da
Cem Aksoylar <caksoylar@gmail.com>
2022-11-09 16:50:49 -0800

    kak: switch to kak-bundle from plug

--- kak/install.sh
+++ kak/install.sh
@@ -3,8 +3,9 @@
 ln -s "$(readlink -f $(dirname $0))" ~/.config/kak
 ln -s "$(readlink -f ~/.config/kak/kak-lsp)" ~/.config/kak-lsp
 
-mkdir -p ~/.config/kak/plugins
+mkdir -p ~/.config/kak/bundle/plugins
 
-git clone https://github.com/robertmeta/plug.kak.git ~/.config/kak/plugins/plug.kak
+git clone https://codeberg.org/jdugan6240/kak-bundle ~/.config/kak/bundle/kak-bundle
+git -C ~/.config/kak/bundle/kak-bundle switch big-rewrite
 
 ln -s $(readlink -f "$(dirname $(type -p kak))"/../share/kak/autoload) ~/.config/kak/autoload/system
