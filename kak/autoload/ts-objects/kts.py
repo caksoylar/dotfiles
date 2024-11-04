bf65182c3b0fab840133dc5559a8676a6397f2b3
Cem Aksoylar <caksoylar@users.noreply.github.com>
2024-11-04 09:54:02 -0800

    Use object mode

--- kak/autoload/ts-objects/kts.py
+++ kak/autoload/ts-objects/kts.py
@@ -12,15 +12,17 @@
 # ///
 
 
+import os
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
-from tree_sitter import Language, Parser
+from tree_sitter import Language, Parser, Node, Point
 
 
 def get_parser_and_queries(language: str) -> tuple[Language, str]:
     """Construct the TS parser and fetch the object queries for given language string."""
+
     def get_queries(lang: str) -> str:
         file_path = Path(__file__).parent / "queries" / (lang + ".scm")
         if not file_path.exists():
@@ -59,6 +61,77 @@ def get_parser_and_queries(language: str) -> tuple[Language, str]:
     return ts_lang, get_queries(language)
 
 
+def get_captures(language: str, source: str, obj: str, extent: str) -> tuple[list[Node], str]:
+    """Find all nodes matching a capture with the @obj.extent tag."""
+
+    ts_lang, queries = get_parser_and_queries(language)
+
+    tag = f"{obj}.{extent}"
+    if "@" + tag not in queries:
+        sys.stderr.write(f"kts.py: Tag {tag} not supported for {language}\n")
+        return [], tag
+
+    parser = Parser(ts_lang)
+    ts_queries = ts_lang.query(queries)
+    tree = parser.parse(source.encode("utf-8"))
+    return ts_queries.captures(tree.root_node).get(tag, []), tag
+
+
+def find_best_node(to_end: bool, cursor: int, nodes: list[Node]) -> list[Node] | None:
+    """
+    For a coordinate at byte offset `cursor`, find the "nearest" node among `nodes`.
+    This will be the smallest node that either contains the cursor or the next/previous
+    node depending on `to_end`.
+    """
+    candidates = [
+        node for node in nodes if (node.end_byte >= cursor if to_end else node.start_byte <= cursor)
+    ]
+    if not candidates:
+        return None
+
+    if to_end:
+        containing_node = max(
+            (node for node in candidates if node.start_byte <= cursor),
+            key=lambda node: node.start_byte,
+            default=None,
+        )
+    else:
+        containing_node = min(
+            (node for node in candidates if node.end_byte >= cursor),
+            key=lambda node: node.end_byte,
+            default=None,
+        )
+    if containing_node is not None:  # cursor is inside a node
+        return containing_node
+
+    # cursor not in a node, find closest node after/before cursor
+    if to_end:
+        return min(candidates, key=lambda node: node.start_byte)
+    return max(candidates, key=lambda node: node.end_byte)
+
+
+def get_new_selection(
+    selection: str, node: Node | None, extend: bool, to_begin: bool, to_end: bool
+) -> str:
+    """
+    Given current selection, captured node, selection mode and boundary flags, return a kak-style
+    selection description for the new selection.
+    """
+    if node is None:
+        return selection
+
+    def p2c(point: Point) -> str:
+        return f"{point.row + 1}.{point.column + 1}"
+
+    if to_begin and to_end:  # select object mode (<a-i>, <a-a>)
+        return f"{p2c(node.start_point)},{p2c(node.end_point)}"
+
+    anchor, cursor = selection.split(",", maxsplit=1)
+    new_anchor = anchor if extend else cursor
+    new_cursor = p2c(node.start_point) if to_begin else p2c(node.end_point)
+    return f"{new_anchor},{new_cursor}"
+
+
 def main():
     """Parse args and find the objects."""
     ap = ArgumentParser()
@@ -66,38 +139,37 @@ def main():
     ap.add_argument(
         "object", choices=["function", "class", "test", "parameter", "comment", "entry"]
     )
-    ap.add_argument("extent", choices=["inside", "around"])
+    ap.add_argument("object_flags")
+    ap.add_argument("select_mode", choices=["replace", "extend"])
     ap.add_argument("cursor_bytes", type=int, nargs="+")
 
     args = ap.parse_args()
     source = sys.stdin.read()
 
-    ts_lang, queries = get_parser_and_queries(args.language)
+    current_selections = os.environ.get("kak_selections_desc").split()
+    assert len(current_selections) == len(args.cursor_bytes)
 
-    parser = Parser(ts_lang)
-    ts_queries = ts_lang.query(queries)
-    tree = parser.parse(source.encode("utf-8"))
+    object_flags = args.object_flags.split("|")
+    extent = "inside" if "inner" in object_flags else "around"
+    to_begin, to_end = "to_begin" in object_flags, "to_end" in object_flags
+    assert to_begin or to_end
 
-    tag = f"{args.object}.{args.extent}"
-    if "@" + tag not in queries:
-        sys.stderr.write(f"kts.py: Tag {tag} not supported for {args.language}\n")
-        return
-
-    captures = ts_queries.captures(tree.root_node)
-
-    capture_boundaries = []
+    captures, tag = get_captures(args.language, source, args.object, extent)
+    capture_nodes = []
     for cursor_byte in args.cursor_bytes:
-        for capture in captures.get(tag, []):
-            if capture.start_byte <= cursor_byte <= capture.end_byte:
-                capture_boundaries.append((capture.start_point, capture.end_point))
-                break
-        else:
-            sys.stderr.write(f"kts.py: No capture found for {tag} around {cursor_byte}\n")
-    if capture_boundaries:
+        try:
+            node = find_best_node(to_end, cursor_byte, captures)
+        except StopIteration:
+            sys.stderr.write(f"kts.py: No appropriate capture found for {tag}\n")
+            node = None
+        capture_nodes.append(node)
+
+    if capture_nodes:
         print("select", end="")
-    for start, end in capture_boundaries:
+    for selection, node in zip(current_selections, capture_nodes):
+        print(" ", end="")
         print(
-            f" {start.row + 1}.{start.column + 1},{end.row + 1}.{end.column + 1}",
+            get_new_selection(selection, node, args.select_mode == "extend", to_begin, to_end),
             end="",
         )
 
--- kak/autoload/ts-objects/ts-objects.kak
+++ kak/autoload/ts-objects/ts-objects.kak
@@ -4,7 +4,7 @@ declare-option -hidden str-list tsobj_selections
 declare-option -hidden int tsobj_prev_timestamp 0
 declare-option -hidden str tsobj_script %sh{printf "%s/kts.py" "$(dirname "$kak_source")"}  # python script path
 
-define-command ts-objects-select -params 3 %{
+define-command ts-objects-select -params 2 %{
     set-option window tsobj_offsets
     evaluate-commands -itersel %{
         set-option -add window tsobj_offsets %val{cursor_byte_offset}
@@ -27,10 +27,11 @@ define-command ts-objects-select -params 3 %{
     evaluate-commands -save-regs p %{
         try %{
             echo -debug %sh{
-                echo pipx run "$kak_opt_tsobj_script" $1 $2 $3 $kak_opt_tsobj_offsets '<'"$kak_opt_tsobj_buf"
+                echo pipx run "$kak_opt_tsobj_script" $1 $2 $kak_object_flags $kak_select_mode $kak_opt_tsobj_offsets '<'"$kak_opt_tsobj_buf"
             }
             evaluate-commands %sh{
-                pipx run "$kak_opt_tsobj_script" $1 $2 $3 $kak_opt_tsobj_offsets <"$kak_opt_tsobj_buf"
+                # kak_selections_desc is used in script
+                pipx run "$kak_opt_tsobj_script" $1 $2 $kak_object_flags $kak_select_mode $kak_opt_tsobj_offsets <"$kak_opt_tsobj_buf"
             }
         } catch %{
             set-register p %opt{tsobj_selections}
@@ -39,15 +40,9 @@ define-command ts-objects-select -params 3 %{
     }
 }
 
-declare-user-mode ts-objects
-map global ts-objects f ':ts-objects-select %opt{filetype} function inside<ret>'  -docstring "function inside"
-map global ts-objects F ':ts-objects-select %opt{filetype} function around<ret>'  -docstring "function around"
-map global ts-objects c ':ts-objects-select %opt{filetype} class inside<ret>'     -docstring "class inside"
-map global ts-objects C ':ts-objects-select %opt{filetype} class around<ret>'     -docstring "class around"
-map global ts-objects t ':ts-objects-select %opt{filetype} test inside<ret>'      -docstring "test inside"
-map global ts-objects T ':ts-objects-select %opt{filetype} test around<ret>'      -docstring "test around"
-map global ts-objects p ':ts-objects-select %opt{filetype} parameter inside<ret>' -docstring "parameter inside"
-map global ts-objects o ':ts-objects-select %opt{filetype} comment inside<ret>'   -docstring "comment inside"
-map global ts-objects O ':ts-objects-select %opt{filetype} comment around<ret>'   -docstring "comment around"
-map global ts-objects e ':ts-objects-select %opt{filetype} entry inside<ret>'     -docstring "entry inside"
-map global ts-objects E ':ts-objects-select %opt{filetype} entry around<ret>'     -docstring "entry around"
+map global object f '<a-semicolon>ts-objects-select %opt{filetype} function<ret>'  -docstring "tree-sitter function"
+map global object c '<a-semicolon>ts-objects-select %opt{filetype} class<ret>'     -docstring "tree-sitter class"
+map global object t '<a-semicolon>ts-objects-select %opt{filetype} test<ret>'      -docstring "tree-sitter test"
+map global object m '<a-semicolon>ts-objects-select %opt{filetype} parameter<ret>' -docstring "tree-sitter parameter"
+map global object o '<a-semicolon>ts-objects-select %opt{filetype} comment<ret>'   -docstring "tree-sitter comment"
+map global object e '<a-semicolon>ts-objects-select %opt{filetype} entry<ret>'     -docstring "tree-sitter entry"
