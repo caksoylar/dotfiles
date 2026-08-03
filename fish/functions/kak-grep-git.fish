d8a40f82b7b4608d3d6e4da672d1e0acef40c568
Cem Aksoylar <caksoylar@users.noreply.github.com>
2026-08-03 21:25:06 +0000

    fish: use git-grep directly for kgg

--- fish/functions/kak-grep-git.fish
+++ fish/functions/kak-grep-git.fish
@@ -3,9 +3,5 @@ function kak-grep-git
     for x in $argv
         set -a grepargs (echo $x | sed -e "s/'/''/g" -e "s/^/'/" -e "s/\$/'/")
     end
-    set -l fileargs
-    for x in (git ls-files)
-        set -a fileargs (echo $x | sed -e "s/'/''/g" -e "s/^/'/" -e "s/\$/'/")
-    end
-    kak -e "grep -0 $(string join -- " " $grepargs) -- $(string join -- " " $fileargs)"
+    kak -e "set-option global grepcmd 'git grep -n --column'; grep $(string join -- " " $grepargs)"
 end
